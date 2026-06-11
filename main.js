2983b87db5c2445a8a5bf638e85cc13ab2d163e6
13hgk20241N <helena.kwiatkoski@escola.pr.gov.br>
2026-06-11 14:19:28 +0000

    aula 4

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 
 for (let i = 0; i < botoes.length; i++) {
   botoes[i].onclick = function () {
@@ -21,3 +22,5 @@ for (let i = 0; i < botoes.length; i++) {
     textos[i].classList.add("ativo");
   };
 }
+    botoes[j].classList.remove("ativo");
+    textos[j].classList.remove("ativo");
